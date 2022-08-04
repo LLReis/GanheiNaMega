0fd4370fb3cd0169e9eeaf93c23fb9ee07f044f4
Lucas Reis <lreis091@gmail.com>
2022-08-04 19:02:56 -0300

    Adicionado persistência de dados da ultima aposta feita e algumas anotações

--- app/src/main/java/co/llreis/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/llreis/ganheinamega/MainActivity.kt
@@ -1,5 +1,7 @@
 package co.llreis.ganheinamega
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,16 +14,26 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Aqui onde começa...
         setContentView(R.layout.activity_main)
 
+
         //Buscar os objetos e referências
         val editText: EditText = findViewById(R.id.edit_number)
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val result = prefs.getString("result", null)
+        if (result != null) {
+            txtResult.text = "Ultima aposta: $result"
+        }
 
         btnGenerate.setOnClickListener {
 
@@ -48,20 +60,29 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-            val numbers = mutableSetOf<Int>()
-            val random = Random()
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
 
-            while (true) {
-                val number = random.nextInt(60)
-                numbers.add(number + 1)
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
 
-                if (numbers.size == qtd) {
-                    break
-                }
+            if (numbers.size == qtd) {
+                break
             }
+        }
 
-            txtResult.text = numbers.joinToString(" - ")
+        txtResult.text = numbers.joinToString(" - ")
 
+        val editor = prefs.edit()
+        editor.putString("result", txtResult.text.toString())
+        editor.apply()
+
+        //editor.commit -> salva de forma sincrona (bloqueia a interface)
+        //                 retorna um boolean informando se teve sucesso ou não
+
+        //editor.apply -> salva de forma asincrona ( não bloqueia a interface)
+        //                não informa se teve sucesso ou não "recomendado"
 
 
     }
